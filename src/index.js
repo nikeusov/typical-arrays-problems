6869aaff92110163aefcca84cdb17dca558adfb4
nikeusov <nikotin331765115@gmail.com>
2021-08-11 19:19:09 +0300

    feat: add content function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (arguments.length === 0 || array.length === 0) {
+    return 0;
+  }
+
+  let min = array[0];
+
+  array.forEach((el) => {if (el < min) min = el })
+
+  return min;
+  }
 
 exports.max = function max (array) {
-  return 0;
+  if (arguments.length === 0 || array.length === 0) {
+    return 0;
+  }
+
+    let max = array[0];
+
+    array.forEach((el) => {if (el > max) max = el })
+
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (arguments.length === 0 || array.length === 0) {
+    return 0;
+  }
+  
+  let summ = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    summ += array[i];
+  }
+
+  return summ / array.length;
 }
